46a907443ae7c27548c11a7e3b40540eac939a5f
z9612 <50051891+z9612@users.noreply.github.com>
2022-01-28 23:30:21 +0900

    Refactor: Integer.compare() 로 리팩토링

--- FastCamp/03~04-정렬/10825. 국영수.java
+++ FastCamp/03~04-정렬/10825. 국영수.java
@@ -1,25 +1,11 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class _10825_국영수 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         Student[] students = new Student[N];
-
-        //12
-        //Junkyu 50 60 100  이름 국 영 수
-        //Sangkeun 80 60 50
-        //Sunyoung 80 70 100
-        //Soong 50 60 90
-        //Haebin 50 60 100
-        //Kangsoo 60 80 100
-        //Donghyuk 80 60 100
-        //Sei 70 70 70
-        //Wonseob 70 70 90
-        //Sanghyun 70 70 80
-        //nsj 80 80 80
-        //Taewhan 50 60 90
 
         for (int i = 0; i < N; i++) {
             students[i] = new Student(sc.next(), sc.nextInt(), sc.nextInt(), sc.nextInt());
@@ -84,9 +70,9 @@ public class _10825_국영수 {
         //모든 점수가 같으면 이름이 사전 순으로 증가하는 순서로 (단, 아스키 코드에서 대문자는 소문자보다 작으므로 사전순으로 앞에 온다.)
         public int compareTo(Student o) {
             if (o.kor == this.kor && o.eng == this.eng && o.math == this.math) return this.name.compareTo(o.name);
-            if (o.kor == this.kor && o.eng == this.eng) return o.math - this.math;
-            if (o.kor == this.kor) return this.eng - o.eng;
-            return o.kor - this.kor;
+            if (o.kor == this.kor && o.eng == this.eng) return Integer.compare(o.math, this.math);
+            if (o.kor == this.kor) return Integer.compare(this.eng, o.eng);
+            return Integer.compare(o.kor, this.kor);
         }
     }
 }
