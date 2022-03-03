f6eee645b9e779432b153ffbdbe6ed3c0470630b
z9612 <50051891+z9612@users.noreply.github.com>
2022-03-03 09:16:54 +0900

    11052. 카드 구매하기

---
+++ Baekjoon/codeplus 예제/알고리즘 기초1/400. DP 1/11052. 카드 구매하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();//10
+        int[] input = new int[N+1]; //1 1 2 3 5 8 13 21 34 55
+        int[] dp = new int[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            input[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i <= N ; i++) {
+            for (int j = 1; j <= i ; j++) {
+                dp[i] = Math.max(dp[i],dp[i-j]+input[j]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
